5e1f8aaa71430e1a0b1ad70a8ae1c7cb3a61398a
garr4561 <168783126+garr4561@users.noreply.github.com>
2024-05-09 08:34:21 -0700

    Update cp-events.js

--- cp-events.js
+++ cp-events.js
@@ -2,26 +2,4 @@ $(document).ready(function() {
 
 	console.log("Events loaded_v1");
 
-	    		window.parent.cpAPIEventEmitter.addEventListener("CPAPI_QUESTIONSUBMIT", function(evt)
-			{
-				var slideID = $('[id^=Slide]').attr("id");
-				console.log("Slide ID: " + slideID);
-				
-				// var quizObject = window.parent.cp.getQuestionObject(slideID);
-
-				// var question_text = quizObject.questionData.qt;
-
-				// var q_status = evt.Data.questionAnsweredCorrectly;
-				// var q_num = evt.Data.questionNumber + 1;
-
-				// var question_obj = {
-				// 	"slideID" : slideID,
-				// 	"questionNumber" : q_num,
-				// 	"questionText" : question_text,
-				// 	"answeredCorrectly" : q_status
-				// };
-
-				// customA_Array.push(question_obj);
-
-			});
 });
