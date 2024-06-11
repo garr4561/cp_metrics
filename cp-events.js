2624e38eaea7d03f994ff020e18041f99ab980a3
garr4561 <168783126+garr4561@users.noreply.github.com>
2024-06-11 10:07:59 -0700

    Update cp-events.js

--- cp-events.js
+++ cp-events.js
@@ -1,10 +1,4 @@
 $(document).ready(function() {
-	console.log("External loaded");
-	
-	window.parent.cpAPIEventEmitter.addEventListener("CPAPI_QUESTIONSUBMIT", function(evt)
-{
-	alert("question answered");
 
-});
 
 });
